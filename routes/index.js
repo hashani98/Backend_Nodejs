f49f0c930ec9da1634e1cf4c2759079b36ffb51c
Pasan Madushan <pasanmadushan@gmail.com>
2021-09-12 15:55:47 +0530

    Itinerary control

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const userRouter = require("./user");
+const apiRouter = require("./api");
+// const itinRouter = require("./itinerary");
 
-router.use("/user", require("./user"));
+router.use("/user", userRouter);
 //router.use("/schedule", require("./schedule"));
-router.use("/apiuser", require("./api"));
+router.use("/apiuser", apiRouter);
+
+// router.use("/itinerary", itinRouter);
 
 module.exports = router;
